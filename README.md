8255c7cf20807dbf1d9a861283080c864308bba4
ColMugX <colmugx@qq.com>
2017-04-21 00:50:47 +0800

    first commit - version 1.1

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # hexo-theme-Slvi
+
+> A simple theme named Slvi for hexo.
+
+目前我自己最满意的主题，就这个主题一直维护下去了。什么各种奇技淫巧都要用了。（但愿HEXO能一直活下去…
+
+[在线预览](https://colmugx.github.io/PersonalBlog)
+
+![Slvi-desktop](https://github.com/ColMugX/GitBed/raw/master/blog/slvi-desktop.jpg)
+
+## 特点 Special
+
+- 使用了动画库 [Syuanpi.css](https://colmugx.github.io/Syuanpi.css)（极轻，未来将支持更多内容
+- 动画可以关闭
+- 使用了lightbox（并没有随波逐流fancybox
+- 文章打赏，打赏自定义
+- 版权说明，协议自定义
+- 五颜六色（逃
+- 可定制范围大
+
+即将支持
+- 多种文章字体
+
+## 下载 Download
+
+1. `git clone theme-rousy https://github.com/ColMugX/hexo-theme-Rousy.git`
+
+2.  `Download ZIP`
+
+3.  `Open in Desktop`
+
+## 安装 Install
+
+放到你的hexo的theme里面，修改你hexo的_config.yml里面的theme
+
+```yaml
+# Extensions
+## Plugins: https://hexo.io/plugins/
+## Themes: https://hexo.io/themes/
+theme: Slvi
+```
+
+## 配置 Setting
+
+要把_source文件夹里面的about拷贝到项目中的source中，**这点很重要！**
+
+在主题的`_config.yml`应该没什么好配置的。比较浅显易懂。
+
+有需要就单独wiki吧。
+
+## 其他平台图片 Picture
+![Slvi-tablet](https://github.com/ColMugX/GitBed/raw/master/blog/slvi-tablet.jpg)
+
+平板版本
+
+![Slvi-mobile](https://github.com/ColMugX/GitBed/raw/master/blog/slvi-mobile.jpg)
+
+手机版本
+
+### 支持 Support
+
+有什么对这个主题看法或者指教可以直接issue。
+
+或者直接到知乎打我！
+
---
+++ source/script/slvi.js
@@ -0,0 +1,60 @@
+(function() {
+    "use strict"
+    var Slvi = {
+        tagcloud() {
+            $('#tags').click(function() {
+                $('.body').each(function() {
+                    if ($(this).hasClass('show-tag')) {
+                        $(this).removeClass('show-tag')
+                    } else {
+                        $(this).addClass('show-tag')
+                    }
+                })
+            })
+        },
+
+        offAnimate() {
+            $('.syuanpi').removeClass('syuanpi')
+        },
+
+        onPicBox () {
+            $('.post-content').each(function() {
+                $(this).find('img').each(function() {
+                    $(this).replaceWith("<a href='" + this.src + "' data-title='" + this.alt + "' data-lightbox='group'><img src='" + this.src + "' alt='" + this.alt + "'></a>")
+                })
+            })
+        },
+
+        back2top() {
+            $(window).each(function() {
+                $(this).scroll(function() {
+                    if($(this).scrollTop() > 110) {
+                        $('#backtop').addClass('bloom').removeClass('dead')
+                    } else {
+                        $('#backtop').addClass('dead').removeClass('bloom')
+                    }
+                });
+            });
+
+            $('#backtop').click(function() {
+                $('body').animate({ scrollTop: 0 })
+            })
+        },
+
+        showComments() {
+            $('#com-switch').click(function() {
+                if ($('#post-comments').css('display') == 'none') {
+                    $('#post-comments').css('display', 'block').addClass('syuanpi fallIn-light')
+                    $(this).removeClass('syuanpi').css('transform', 'rotate(180deg)')
+                } else {
+                    $(this).addClass('syuanpi').css('transform', '')
+                    $('#post-comments').removeClass('fallIn-light').addClass('riseOut-light').one('webkitAnimationEnd AnimationEnd', function() {
+                        $(this).removeClass('syuanpi riseOut-light').css('display', 'none')
+                    })
+                }
+            })
+        }
+    }
+
+    this.Slvi = Slvi
+}.call(this))
