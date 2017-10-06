cf0688d12336a43322a475c3d799d85793aeb6c1
ColMugX <colmugx@qq.com>
2017-10-06 16:02:55 +0800

    pic position

--- source/script/nlvi.js
+++ source/script/nlvi.js
@@ -1,23 +1,29 @@
-(function() {
+(function () {
     "use strict"
     var Nlvi = {
         tagcloud() {
-            $('#tags').click(function() {
-                $('#tagcloud').css({'transform': 'translateY(0px)', '-webkit-transform': 'translateY(0px)'})
-                $('.tagcloud-mask').css({'background': 'rgba(0, 0, 0, 0.3)', 'z-index': '500'})
+            $('#tags').click(function () {
+                $('#tagcloud').css({
+                    'transform': 'translateY(0px)',
+                    '-webkit-transform': 'translateY(0px)'
+                })
+                $('.tagcloud-mask').css({
+                    'background': 'rgba(0, 0, 0, 0.3)',
+                    'z-index': '500'
+                })
             })
-            $('.tagcloud-mask').click(function() {
+            $('.tagcloud-mask').click(function () {
                 $('#tagcloud').css('transform', '').css('-webkit-transform', '')
                 $('.tagcloud-mask').css('background', '').css('z-index', '')
             })
-            $('#mobile-tags').click(function() {
+            $('#mobile-tags').click(function () {
                 $('.inner-cloud').css('transform', 'translateX(-96%)')
             })
         },
 
         mobileHeader() {
             var that = this
-            $(window).scroll(function() {
+            $(window).scroll(function () {
                 if ($(window).scrollTop() > $('#header').height()) {
                     $('.mobile-header').css('top', $('.mobile-header-nav').height() - $('.mobile-header').height())
                     $('#mobile-left').removeClass('item-clicked')
@@ -27,7 +33,7 @@
                     $('#tagcloud').show()
                 }
             })
-            $('#mobile-left').click(function() {
+            $('#mobile-left').click(function () {
                 if ($(this).hasClass('item-clicked')) {
                     return that.closeHeaderMenu()
                 }
@@ -48,32 +54,42 @@
             $('.syuanpi').removeClass('syuanpi')
         },
 
-        onPicBox () {
-            $('.post-content').each(function() {
-                $(this).find('img').each(function() {
+        picPos() {
+            $('.post-content').each(function () {
+                $(this).find('img').each(function () {
+                    $(this).parent('p').css('text-align', 'center')
+                })
+            })
+        },
+
+        onPicBox() {
+            $('.post-content').each(function () {
+                $(this).find('img').each(function () {
                     $(this).replaceWith("<a href='" + this.src + "' data-title='" + this.alt + "' data-lightbox='group'><img src='" + this.src + "' alt='" + this.alt + "'></a>")
                 })
             })
         },
 
         smoothScroll() {
-            $('.toc-link').click(function() {
-                $('body').animate({ scrollTop: $($.attr(this, 'href')).offset().top - 200 })
+            $('.toc-link').click(function () {
+                $('body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top - 200
+                })
             })
         },
 
         onView() {
             var title = document.title
             var tme
-            document.addEventListener('visibilitychange', function() {
+            document.addEventListener('visibilitychange', function () {
                 var sct = Math.floor($(window).scrollTop() / ($(document).height() - $(window).height()) * 100)
                 if ($(document).height() - $(window).height() === 0) sct = 100
                 if (document.hidden) {
                     clearTimeout(tme)
-                    document.title = 'Read '+sct+'% · '+title
+                    document.title = 'Read ' + sct + '% · ' + title
                 } else {
                     document.title = 'Welcome Back · ' + title
-                    tme = setTimeout(function() {
+                    tme = setTimeout(function () {
                         document.title = title
                     }, 3000)
                 }
@@ -81,9 +97,9 @@
         },
 
         back2top() {
-            $(window).each(function() {
-                $(this).scroll(function() {
-                    if($(this).scrollTop() > 110) {
+            $(window).each(function () {
+                $(this).scroll(function () {
+                    if ($(this).scrollTop() > 110) {
                         $('#backtop').addClass('bloom').removeClass('dead')
                     } else {
                         $('#backtop').addClass('dead').removeClass('bloom')
@@ -93,23 +109,25 @@
                     var docHeight = $(document).height()
                     var winHeight = $(window).height()
                     var scrollPercent = (scrollTop) / (docHeight - winHeight)
-                    var scrollPercentRounded = Math.floor(scrollPercent*100)
+                    var scrollPercentRounded = Math.floor(scrollPercent * 100)
                     $('#scrollpercent').html(scrollPercentRounded)
                 })
             })
-            $('.toTop').click(function() {
-                $('html, body').animate({ scrollTop: 0 })
+            $('.toTop').click(function () {
+                $('body').animate({
+                    scrollTop: 0
+                })
             })
         },
 
         showComments() {
-            $('#com-switch').click(function() {
+            $('#com-switch').click(function () {
                 if ($('#post-comments').css('display') == 'none') {
                     $('#post-comments').css('display', 'block').addClass('syuanpi fallIn-light')
                     $(this).removeClass('syuanpi').css('transform', 'rotate(180deg)')
                 } else {
                     $(this).addClass('syuanpi').css('transform', '')
-                    $('#post-comments').removeClass('fallIn-light').addClass('riseOut-light').one('webkitAnimationEnd AnimationEnd', function() {
+                    $('#post-comments').removeClass('fallIn-light').addClass('riseOut-light').one('webkitAnimationEnd AnimationEnd', function () {
                         $(this).removeClass('syuanpi riseOut-light').css('display', 'none')
                     })
                 }
@@ -118,10 +136,10 @@
 
         showReward() {
             $('#reward-btn').click(function () {
-                if($('#reward-wrapper').css('display') == 'none') {
+                if ($('#reward-wrapper').css('display') == 'none') {
                     $('#reward-wrapper').css('display', 'flex').addClass('syuanpi fallIn-light')
                 } else {
-                    $('#reward-wrapper').removeClass('fallIn-light').addClass('riseOut-light').one('webkitAnimationEnd AnimationEnd', function() {
+                    $('#reward-wrapper').removeClass('fallIn-light').addClass('riseOut-light').one('webkitAnimationEnd AnimationEnd', function () {
                         $(this).removeClass('syuanpi riseOut-light').css('display', 'none')
                     })
                 }
@@ -139,7 +157,7 @@
                     return $(link).offset().top
                 })
                 $('.title-link a').each(function () {
-                    if($scrollTop >= 0 && $scrollTop < 230) {
+                    if ($scrollTop >= 0 && $scrollTop < 230) {
                         $(this).addClass('active')
                     } else {
                         $(this).removeClass('active')
